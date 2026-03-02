b2098cb25b6affd6dd492251676b47394698bf35
Nguyen Ngoc Khanh <nguyenng@a2ap-login02.asp2p.nscc.sg>
2026-03-02 14:04:08 +0800

    auto_commit

--- upload_huggingface_calculator.py
+++ upload_huggingface_calculator.py
@@ -1,3 +1,3 @@
-from huggingface_hub import login, upload_folder
+from huggingface_hub import login, upload_large_folder
 login()
-upload_folder(folder_path="mnt/output/calculator", repo_id="khanh2023/qwen3-0.6b-calculator", repo_type="model")
+upload_large_folder(folder_path="mnt/output/calculator", repo_id="khanh2023/qwen3-0.6b-calculator", repo_type="model")
