ea823f038779258ba4663787e0ea1c209c994874
khanh <khanh.nguyen.contact@gmail.com>
2026-03-05 16:00:02 +0800

    auto_commit

--- leanlm/model.py
+++ leanlm/model.py
@@ -10,9 +10,9 @@ type ModelForCausalLM = Any
 from pydantic import BaseModel
 
 class TextToText(BaseModel):
-    get_prompt_from_input: Callable[[str], str]
-    get_input_from_prompt: Callable[[str], str]
-    get_output_from_completion: Callable[[str], str]
+    get_input_from_question: Callable[[str], str]
+    get_question_from_input: Callable[[str], str]
+    get_answer_from_output: Callable[[str], str]
 
     model_path: str
     lora_checkpoint_path: str | None = None
@@ -43,15 +43,45 @@ class TextToText(BaseModel):
         
         return self._model
 
+    def decode(self, questions: list[str]) -> list[str]:
+        inputs = [self.get_input_from_question(question) for question in questions]
+
+        input_ids = self.tokenizer(
+            inputs,
+            return_tensors="pt",
+            padding=True,
+        )
+        output_ids = self.model.generate(
+            **input_ids,
+
+            # hard code
+            max_new_tokens=32768,
+            temperature=0.6,
+            top_p=0.95,
+            top_k=20,
+            min_p=0.0,
+            repetition_penalty=1.0,
+        )
+        outputs = self.tokenizer.batch_decode(
+            output_ids,
+        )
+        answers = [self.get_answer_from_output(output) for output in outputs]
+
+        return answers
+
+
 if __name__ == "__main__":
     t2t = TextToText(
-        get_prompt_from_input=lambda input_str: f"<|im_start|>user\n{input_str}<|im_end|>\n<|im_start|>assistant\n<think>\n",
-        get_input_from_prompt=lambda prompt: prompt.lstrip("<|im_start|>user\n").rstrip("<|im_end|>\n<|im_start|>assistant\n<think>\n"),
-        get_output_from_completion=lambda completion: completion.split("</think>")[-1],
+        get_input_from_question=lambda input_str: f"<|im_start|>user\n{input_str}<|im_end|>\n<|im_start|>assistant\n<think>\n",
+        get_question_from_input=lambda prompt: prompt.lstrip("<|im_start|>user\n").rstrip("<|im_end|>\n<|im_start|>assistant\n<think>\n"),
+        get_answer_from_output=lambda completion: completion.split("</think>")[-1],
         model_path="Qwen/Qwen3-0.6B",
     )
 
-    print(t2t.tokenizer)
-    print(t2t.model)
+    answers = t2t.decode([
+        "What is 2 + 2?",
+        "Compute 17 * 19."
+    ])
+    print(answers)
 
     
