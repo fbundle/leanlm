36c8df31b1576d945d2c6e1dce3c5f830c851d81
khanh <khanh.nguyen.contact@gmail.com>
2026-03-07 16:59:05 +0800

    auto_commit

--- download_huggingface_calculator.py
+++ download_huggingface_calculator.py
@@ -2,7 +2,7 @@ from huggingface_hub import snapshot_download
 
 
 OUTPUT_DIR = "mnt/output/calculator_qwen3_0p6b_lora_v1"
-REPO_ID = "khanh2023/qwen3-0.6b-lora-calculator"
+REPO_ID = "khanh2023/qwen3-0.6b-v1-lora-calculator"
 
 snapshot_download(
     local_dir=OUTPUT_DIR,
