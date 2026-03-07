716454373a7c3f167209950d804265e09968624d
khanh <khanh.nguyen.contact@gmail.com>
2026-03-07 17:02:24 +0800

    auto_commit

--- upload_huggingface_calculator.py
+++ upload_huggingface_calculator.py
@@ -1,7 +1,7 @@
 from huggingface_hub import login, upload_large_folder
 
 OUTPUT_DIR = "mnt/output/calculator_qwen3_0p6b_lora_v1"
-REPO_ID = "khanh2023/qwen3-0.6b-v1-lora-calculator"
+REPO_ID = "khanh2023/qwen3-0.6b-lora-calculator"
 
 login()
 upload_large_folder(
