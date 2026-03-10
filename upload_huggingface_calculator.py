5ff03e8f2da1626d61c78e67f56a3ef289f2eb7e
khanh <khanh.nguyen.contact@gmail.com>
2026-03-10 14:15:48 +0800

    auto_commit

--- upload_huggingface_calculator.py
+++ upload_huggingface_calculator.py
@@ -1,7 +1,15 @@
+import os
+import shutil
 from huggingface_hub import login, upload_large_folder
 
 OUTPUT_DIR = "mnt/output/calculator_qwen3_0p6b_lora_v1"
 REPO_ID = "khanh2023/qwen3-0.6b-lora-calculator"
+CODE = "leanlm"
+
+if os.path.exists(f"{OUTPUT_DIR}/{CODE}"):
+    shutil.rmtree(f"{OUTPUT_DIR}/{CODE}")
+
+shutil.copytree(CODE, f"{OUTPUT_DIR}/{CODE}")
 
 login()
 upload_large_folder(
