3fd4d0f0b531fb8bd96a7498027090d4ba4cb297
khanh <khanh.nguyen.contact@gmail.com>
2026-03-12 15:43:44 +0800

    auto_commit

--- examples/demo_lean4.py
+++ examples/demo_lean4.py
@@ -2,15 +2,10 @@ from lean_dojo import *
 
 repo = LeanGitRepo(
     "https://github.com/leanprover-community/mathlib4",
-    "8f9d9cff6bd728b17a24e163c9402775d9e6a365",
+    "v4.21.0",
 )
 
 
-
-repo = LeanGitRepo(
-    "/Users/khanh/code/lean4_example/theorem_proving",
-    "master",
-)
 
 print(repo.get_config("lean-toolchain"))
 
