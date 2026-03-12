ca3177be622bbedc17de7a28e9560bb9e64529fe
khanh <khanh.nguyen.contact@gmail.com>
2026-03-12 18:47:48 +0800

    auto_commit

--- lab/demo_lean4.py
+++ lab/demo_lean4.py
@@ -2,7 +2,7 @@ from lean_dojo import LeanGitRepo, trace
 
 repo = LeanGitRepo(
     "git@github.com:fbundle/lean4-example.git",
-    "v1.0",
+    "master",
 )
 
 print(repo.get_config("lean-toolchain"))
