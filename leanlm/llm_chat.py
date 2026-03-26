5f239440711bafd3590d462953656ebd29e414da
khanh <khanh.nguyen.contact@gmail.com>
2026-03-26 16:55:49 +0800

    auto_commit

--- leanlm/llm_chat.py
+++ leanlm/llm_chat.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+
 from typing import Iterator, Any
+import os
+
 
 from threading import Thread
 from pydantic import BaseModel
@@ -59,16 +62,27 @@ class TransformerStreamer(Streamer):
         return streamer()
 
 class Conversation:
-    def __init__(self, initial_message_list: list[Message] | None = None):
+    def __init__(self, path: str):
+        self.path = path
         self.message_list = []
-        if initial_message_list is not None:
-            self.message_list.extend(initial_message_list)
+        if not os.path.exists(path):
+            os.makedirs(os.path.dirname(path))
+            open(path, mode="w").close()
+        
+        with open(path) as f:
+            for line in f:
+                message = Message.model_validate_json(line)
+                self.message_list.append(message)
     
     def append(self, role: str, content: str) -> Conversation:
-        self.message_list.append(Message(
+        message = Message(
             role=role, 
             content=content,
-        ))
+        )
+        self.message_list.append(message)
+        with open(self.path, mode="w") as f:
+            f.write(message.model_dump_json() + "\n")
+        
         return self
 
     def append_user_message(self, content: str) -> Conversation:
