2cbf6598d210148e152cfe162bf1f67777f78bb6
khanh <khanh.nguyen.contact@gmail.com>
2026-03-26 22:32:32 +0800

    auto_commit

--- convert_checkpoint.py
+++ convert_checkpoint.py
@@ -1,5 +1,18 @@
 import sys
 
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+def load_model_and_tokenizer(model_path: str, peft_path: str):
+    tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=model_path,
+    )
+    base_model = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=model_path,
+    )
+    model = PeftModel.from_pretrained(base_model, peft_path)
+    return model, tokenizer
+
+
 if __name__ == "__main__":
     model_path, peft_path = sys.argv[1], sys.argv[2]
     print(model_path, peft_path)
