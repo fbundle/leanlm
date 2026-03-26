eeb1679bfc1dec27e3116351c4fd9d7b38fe6df7
khanh <khanh.nguyen.contact@gmail.com>
2026-03-26 22:41:48 +0800

    auto_commit

--- convert_checkpoint.py
+++ convert_checkpoint.py
@@ -1,7 +1,9 @@
 import sys
+import os
 import shutil
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
 
 def load_model(model_path: str, peft_path: str):
     tokenizer = AutoTokenizer.from_pretrained(
@@ -17,7 +19,7 @@ def ensure_dir(path: str):
     dirname = os.path.dirname(path)
     if not os.path.exists(dirname):
         os.makedirs(dirname)
-    if os.path.exists(path)
+    if os.path.exists(path):
         shutil.rmtree(path)
     
 def save_model_hf(path: str, model, tokenizer):
@@ -28,8 +30,13 @@ def save_model_hf(path: str, model, tokenizer):
 
 if __name__ == "__main__":
     model_path, peft_path = sys.argv[1], sys.argv[2]
+    peft_path = os.path.abspath(peft_path)
+
     model, tokenizer = load_model(model_path, peft_path)
 
-    model_name = os.path.basename(os.path.dirname(peft_path))
+    prefix = os.path.dirname(os.path.dirname(peft_path))
+    peft_name = peft_path.lstrip(prefix).replace("/", "_")
 
-    save_model_hf(f"mnt/output_hf/{model_name}", model, tokenizer)
+    print(peft_name)
+
+    # save_model_hf(f"mnt/output_hf/{model_name}", model, tokenizer)
