830a0b52efcaa4ba0a34d75e4a59dc3a4f9e63ab
khanh <khanh.nguyen.contact@gmail.com>
2026-03-26 22:48:22 +0800

    auto_commit

--- convert_checkpoint.py
+++ convert_checkpoint.py
@@ -5,20 +5,27 @@ import shutil
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from peft import PeftModel
 
-def load_model(model_path: str, peft_path: str):
+def load_model(model_path: str, peft_path: str | None):
+    peft_path = os.path.abspath(peft_path)
+
     tokenizer = AutoTokenizer.from_pretrained(
         pretrained_model_name_or_path=model_path,
     )
     base_model = AutoModelForCausalLM.from_pretrained(
         pretrained_model_name_or_path=model_path,
     )
-    model = PeftModel.from_pretrained(base_model, peft_path)
-    return model, tokenizer
+    if peft_path is None:
+        model_name = os.path.basename(model_path)
+        return base_model, tokenizer, model_name
+    else:
+        prefix = os.path.dirname(os.path.dirname(peft_path))
+        model_name = peft_path.lstrip(prefix).replace("/", "_")
+
+        model = PeftModel.from_pretrained(base_model, peft_path)
+        model = model.merge_and_unload() # type: ignore
+        return model, tokenizer, model_name
 
 def ensure_dir(path: str):
-    dirname = os.path.dirname(path)
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
     if os.path.exists(path):
         shutil.rmtree(path)
     
@@ -29,14 +36,11 @@ def save_model_hf(path: str, model, tokenizer):
 
 
 if __name__ == "__main__":
-    model_path, peft_path = sys.argv[1], sys.argv[2]
-    peft_path = os.path.abspath(peft_path)
+    model_path = sys.argv[1]
+    peft_path = None
+    if len(sys.argv) >= 3:
+        peft_path = sys.argv[2]
 
-    model, tokenizer = load_model(model_path, peft_path)
+    model, tokenizer, model_name = load_model(model_path, peft_path)
 
-    prefix = os.path.dirname(os.path.dirname(peft_path))
-    peft_name = peft_path.lstrip(prefix).replace("/", "_")
-
-    print(peft_name)
-
-    # save_model_hf(f"mnt/output_hf/{model_name}", model, tokenizer)
+    save_model_hf(f"mnt/output_hf/{model_name}", model, tokenizer)
