efa2a62e3486fef1787c6f28bff4ee5bf053a828
khanh <khanh.nguyen.contact@gmail.com>
2026-03-26 22:16:49 +0800

    auto_commit

--- run_eval_mlx.py
+++ run_eval_mlx.py
@@ -1,6 +1,9 @@
 import os
 
 import mlx_lm
+import mlx_lm.sample_utils
+
+
 
 OUTPUT_DIR = "mnt/output_mlx"
 
@@ -14,7 +17,7 @@ def convert_model(output_dir: str, model_path: str) -> str:
     if os.path.exists(output_path):
         return output_path
     
-    mlx_lm.convert(
+    mlx_lm.convert.convert(
         hf_path=model_path,
         mlx_path=output_path,
         quantize=True,
@@ -22,18 +25,40 @@ def convert_model(output_dir: str, model_path: str) -> str:
 
     return output_path
 
+def enable_thinking(prompt: str) -> str:
+    prompt = prompt.rstrip()
+    prompt = prompt.rstrip("</think>")
+    prompt = prompt.rstrip()
+    prompt = prompt + "\n\n"
+    return prompt
+
 def main(output_dir: str, model_path: str):
     model_path_mlx = convert_model(output_dir, model_path)
-    model, tokenizer = mlx_lm.load(model_path_mlx)
+    parts = mlx_lm.load(path_or_hf_repo=model_path_mlx)
+    if len(parts) != 2:
+        return
+    model, tokenizer = parts
 
-    prompt = "Write a story about Einstein"
-
-    messages = [{"role": "user", "content": prompt}]
+    messages = [{"role": "user", "content": "12345+67890="}]
     prompt = tokenizer.apply_chat_template(
-        messages, add_generation_prompt=True,
+        messages,
+        tokenize=False,
+        add_generation_prompt=True,
+    )
+    prompt = enable_thinking(prompt)
+
+    sampler = mlx_lm.sample_utils.make_sampler(
+        temp=0.6,
+        top_p=0.95,
+        top_k=20,
+        min_p=0.0,
     )
 
-    for response in mlx_lm.stream_generate(model, tokenizer, prompt, max_tokens=512):
+    for response in mlx_lm.stream_generate(
+        model=model, tokenizer=tokenizer, prompt=prompt,
+        max_tokens=32768,
+        sampler=sampler,
+    ):
         print(response.text, end="", flush=True)
     print()
 
