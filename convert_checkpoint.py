096c1436bbc6119d58d2c87f247a95b5390068fb
khanh <khanh.nguyen.contact@gmail.com>
2026-03-27 00:16:48 +0800

    auto_commit

--- convert_checkpoint.py
+++ convert_checkpoint.py
@@ -22,33 +22,11 @@ def repeat(n: int) -> Callable[[Map], Map]:
         return helper2
     return helper1
 
-def load_model(model_path: str, peft_path: str | None):
-    tokenizer = AutoTokenizer.from_pretrained(
-        pretrained_model_name_or_path=model_path,
-    )
-    base_model = AutoModelForCausalLM.from_pretrained(
-        pretrained_model_name_or_path=model_path,
-    )
-    if peft_path is None:
-        model_name = os.path.basename(model_path)
-        return base_model, tokenizer, model_name
-    else:
-        peft_path = os.path.abspath(peft_path).rstrip("/")
-        prefix = repeat(2)(os.path.dirname)(peft_path)
-        model_name = peft_path.lstrip(prefix).replace("/", "_")
-
-        model = PeftModel.from_pretrained(base_model, peft_path)
-        model = model.merge_and_unload() # type: ignore
-        return model, tokenizer, model_name
-
-def ensure_dir(path: str):
-    if os.path.exists(path):
-        shutil.rmtree(path)
-
 
 model_type_patch = {
     "qwen3_5_text": "qwen3_5"
 }
+
 def patch_hf(hf_path: str):
     with open(f"{hf_path}/config.json") as f:
         config = json.loads(f.read())
@@ -57,20 +35,38 @@ def patch_hf(hf_path: str):
     with open(f"{hf_path}/config.json", "w") as f:
         f.write(json.dumps(config, indent=2))
 
-def save_model(hf_path: str, mlx_path: str, model, tokenizer):
-    if not os.path.exists(hf_path):
-        model.save_pretrained(hf_path)
-        tokenizer.save_pretrained(hf_path)
-        patch_hf(hf_path)
-
-    if not os.path.exists(mlx_path):
+def main(model_path: str, peft_path: str | None):
+    if peft_path is None:
+        model_name = os.path.basename(model_path)
         mlx_lm.convert(
-            hf_path=hf_path,
-            mlx_path=mlx_path,
+            hf_path=model_path,
+            mlx_path=f"mnt/output_mlx/{model_name}",
             quantize=True,
         )
+    else:
+        peft_path = os.path.abspath(sys.argv[2]).rstrip("/")
+        prefix = repeat(2)(os.path.dirname)(peft_path)
+        model_name = peft_path.lstrip(prefix).replace("/", "_")
 
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
+        base_model = AutoModelForCausalLM.from_pretrained(model_path)
+        model = PeftModel.from_pretrained(base_model, peft_path)
+        model = model.merge_and_unload() # type: ignore
 
+        hf_path=f"mnt/output_hf/{model_name}",
+        mlx_path=f"mnt/output_mlx/{model_name}",
+
+        if not os.path.exists(hf_path):
+            model.save_pretrained(hf_path)
+            tokenizer.save_pretrained(hf_path)
+            patch_hf(hf_path)
+
+        if not os.path.exists(mlx_path):
+            mlx_lm.convert(
+                hf_path=hf_path,
+                mlx_path=mlx_path,
+                quantize=True,
+            )
 
 
 if __name__ == "__main__":
@@ -78,15 +74,5 @@ if __name__ == "__main__":
     peft_path = None
     if len(sys.argv) >= 3:
         peft_path = sys.argv[2]
-
-    model, tokenizer, model_name = load_model(model_path, peft_path)
-
-    if model.config.model_type in model_type_patch:
-        model.config.model_type = model_type_patch[model.config.model_type]
-
-    save_model(
-        hf_path=f"mnt/output_hf/{model_name}",
-        mlx_path=f"mnt/output_mlx/{model_name}",
-        model=model,
-        tokenizer=tokenizer,
-    )
+    main(model_path, peft_path)
+        
