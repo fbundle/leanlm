c42e350836b651bc33a1808dd1a60a773df6c091
khanh <khanh.nguyen.contact@gmail.com>
2026-03-27 00:53:19 +0800

    auto_commit

--- llm_chat.py
+++ llm_chat.py
@@ -3,11 +3,17 @@ from __future__ import annotations
 
 import os
 import sys
+from threading import Thread
 import time
+from typing import Any, Iterator
 
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import TextIteratorStreamer
 
 import mlx_lm
 import mlx_lm.sample_utils
+
+
 from pydantic import BaseModel
 
 def enable_thinking(prompt: str) -> str:
@@ -51,6 +57,85 @@ class Conversation:
     def dumps(self) -> list[dict[str, str]]:
         return [message.model_dump() for message in self.message_list]
 
+class Streamer:
+    def chat(self, message_list: list[Message], addon_kwargs: dict[str, Any] | None = None) -> Iterator[str]:
+        raise NotImplemented
+
+class TransformerStreamer(Streamer):
+    def __init__(self, model_path: str):
+        super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.model = AutoModelForCausalLM.from_pretrained(model_path, device_map="auto")
+    
+    def _generate(self, message_list: list[Message], generate_kwargs: dict[str, Any], text_streamer: TextIteratorStreamer):
+        input_text = self.tokenizer.apply_chat_template(
+            conversation=[message.model_dump() for message in message_list],
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+        input_text = enable_thinking(input_text)
+        input_ids = self.tokenizer(input_text, return_tensors="pt").to(self.model.device)
+        self.model.generate(
+            **input_ids,
+            steamer=text_streamer,
+            max_new_tokens=-1,
+            **generate_kwargs,
+        )
+    
+    def chat(self, message_list: list[Message], addon_kwargs: dict[str, Any] | None = None) -> Iterator[str]:
+        text_streamer = TextIteratorStreamer(
+            self.tokenizer,
+            skip_prompt=True,  # skip the prompt, stream the output only
+            skip_special_tokens=True,  # pass into tokenizer.decode, skip EOS for example
+        )
+        generate_kwargs: dict[str, Any] = {}
+        if addon_kwargs is not None:
+            generate_kwargs.update(addon_kwargs)
+
+        thread = Thread(target=TransformerStreamer._generate, args=(self, message_list, generate_kwargs, text_streamer))
+        thread.start()
+
+        def streamer() -> Iterator[str]:
+            yield from text_streamer
+            thread.join()
+
+        return streamer()
+
+class MlxStreamer(Streamer):
+    def __init__(self, model_path: str):
+        super().__init__()
+        model, tokenizer, config = mlx_lm.load( # type: ignore
+            path_or_hf_repo=model_path,
+            return_config=True,
+        )
+        self.model = model
+        self.tokenizer = tokenizer
+    
+    def chat(self, message_list: list[Message], addon_kwargs: dict[str, Any] | None = None) -> Iterator[str]:
+        input_text = self.tokenizer.apply_chat_template(
+            conversation=[message.model_dump() for message in message_list],
+            tokenize=False,
+            add_generation_prompt=True,
+        )
+        input_text = enable_thinking(input_text)
+        generate_kwargs: dict[str, Any] = {}
+        if addon_kwargs is not None:
+            generate_kwargs.update(addon_kwargs)
+
+        sampler = mlx_lm.sample_utils.make_sampler(**generate_kwargs)
+
+        response_generator = mlx_lm.stream_generate(
+            model=self.model, tokenizer=self.tokenizer, prompt=input_text,
+            max_tokens=-1, sampler=sampler,
+        )
+
+        def streamer() -> Iterator[str]:
+            for response in response_generator:
+                yield response.text
+
+        return streamer()
+
+
 WELCOME = "type your prompt (type ':q' to quit) (type ':s <prompt>' to set system prompt)"
 LOOP_PROMPT = ">>>"
 CONVERSATION_PATH = ".chat.jsonl"
@@ -83,7 +168,7 @@ def main(model_path: str):
             try:
                 for response in mlx_lm.stream_generate(
                     model=model, tokenizer=tokenizer, prompt=prompt,
-                    max_tokens=32768,
+                    max_tokens=-1,
                     sampler=sampler,
                 ):
                     text_list.append(response.text)
