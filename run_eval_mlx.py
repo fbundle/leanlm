e20d608ecae9463a78f37dd9b888050fcde86dd4
khanh <khanh.nguyen.contact@gmail.com>
2026-03-26 23:16:48 +0800

    auto_commit

--- run_eval_mlx.py
+++ run_eval_mlx.py
@@ -1,28 +1,7 @@
-import os
+import sys
 
 import mlx_lm
 import mlx_lm.sample_utils
-
-
-
-OUTPUT_DIR = "mnt/output_mlx"
-
-MODEL_PATH = "Qwen/Qwen3-0.6B"
-MODEL_PATH = "Qwen/Qwen3.5-0.8B"
-
-def convert_model(output_dir: str, model_path: str) -> str:
-    model_name = os.path.basename(model_path)
-    output_path = f"{output_dir}/{model_name}"
-    if os.path.exists(output_path):
-        return output_path
-    
-    mlx_lm.convert(
-        hf_path=model_path,
-        mlx_path=output_path,
-        quantize=True,
-    )
-
-    return output_path
 
 def enable_thinking(prompt: str) -> str:
     prompt = prompt.rstrip()
@@ -35,9 +14,8 @@ def enable_thinking(prompt: str) -> str:
 
     return prompt
 
-def main(output_dir: str, model_path: str):
-    model_path_mlx = convert_model(output_dir, model_path)
-    parts = mlx_lm.load(path_or_hf_repo=model_path_mlx)
+def main(model_path: str):
+    parts = mlx_lm.load(path_or_hf_repo=model_path)
     if len(parts) != 2:
         return
     model, tokenizer = parts
@@ -66,4 +44,4 @@ def main(output_dir: str, model_path: str):
     print()
 
 if __name__ == "__main__":
-    main(OUTPUT_DIR, MODEL_PATH)
+    main(sys.argv[1])
