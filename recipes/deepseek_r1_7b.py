227f4f5cbac239d2e4ab0f4f47c9cc6fe8c1797a
khanh <khanh.nguyen.contact@gmail.com>
2026-04-10 16:54:20 +0800

    auto_commit

---
+++ recipes/deepseek_r1_7b.py
@@ -0,0 +1,16 @@
+from llm_trainer.trainer import Processor, Language
+
+class DeepseekR1Processor(Processor):
+    def __init__(self):
+        super().__init__()
+
+    def marshal_input(self, input_text: str) -> Language:
+        return "<｜begin▁of▁sentence｜><｜User｜>" + input_text + "<｜Assistant｜><think>\n"
+
+    def unmarshal_input(self, prompt: Language) -> str:
+        return prompt.lstrip("<｜begin▁of▁sentence｜><｜User｜>").rstrip("<｜Assistant｜><think>\n")
+
+    def unmarshal_output(self, completion: Language) -> str:
+        completion = completion.split("</think>")[-1]
+        completion = completion.split("<｜end▁of▁sentence｜>")[0]
+        return completion
