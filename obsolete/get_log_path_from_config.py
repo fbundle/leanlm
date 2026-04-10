effd2ace9f33efb05fed6718828880cebd7084a7
khanh <khanh.nguyen.contact@gmail.com>
2026-04-10 16:39:31 +0800

    auto_commit

--- obsolete/get_log_path_from_config.py
+++
@@ -1,9 +0,0 @@
-import sys
-import json
-
-def get_log_path(config_path: str) -> str:
-    return json.loads(open(config_path).read())["train"]["log_path"]
-
-if __name__ == "__main__":
-    config_path = sys.argv[1]
-    print(get_log_path(config_path))
