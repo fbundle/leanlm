9fc9ac27489f251b9ecd6b495b5c90384ac2b1d7
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-11 20:20:12 +0800

    auto_commit

--- leanlm/llm_engine/engine.py
+++ leanlm/llm_engine/engine.py
@@ -181,13 +181,10 @@ if __name__ == "__main__":
 
     engine = TransformerEngine("Qwen/Qwen3.5-4B")
 
-    OUTPUT_DIR = "mnt/output/qwen3.5-4b-lora-calculator"
-    REPO_ID = "khanh2023/qwen3.5-4b-lora-calculator"
+    OUTPUT_DIR = "mnt/output/qwen3.5-4b-calculator"
+    REPO_ID = "khanh2023/qwen3.5-4b-calculator"
 
-    snapshot_download(
-        local_dir=OUTPUT_DIR,
-        repo_id=REPO_ID,
-    )
+    # snapshot_download(local_dir=OUTPUT_DIR,repo_id=REPO_ID)
 
     checkpoint = get_last_checkpoint(OUTPUT_DIR)
     engine.model = PeftModel.from_pretrained(engine.model, checkpoint) # type: ignore
