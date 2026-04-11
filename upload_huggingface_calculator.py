f885642727a51deed0af00ed4e9be25dfb7e4d7c
khanh <khanh.nguyen.contact@gmail.com>
2026-04-12 02:39:38 +0800

    auto_commit

--- upload_huggingface_calculator.py
+++ upload_huggingface_calculator.py
@@ -3,18 +3,19 @@ import datetime
 
 
 name = "qwen3.5-4b-length4096-lora-calculator"
-
-folder_path = f"mnt/output/{name}"
-repo_id=f"khanh2023/{name}"
-
 now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z")
-with open(f"{folder_path}/last_poll.txt", "w") as f:
-    f.write(now)
 
-login()
-upload_large_folder(
-    folder_path=folder_path,
-    repo_id=repo_id,
-    repo_type="model",
-)
+for name in ["qwen3.5-4b-length4096-lora-calculator", "qwen3.5-4b-length4096-calculator"]:
+    folder_path = f"mnt/output/{name}"
+    repo_id=f"khanh2023/{name}"
+
+    with open(f"{folder_path}/last_poll.txt", "w") as f:
+        f.write(now)
+
+    login()
+    upload_large_folder(
+        folder_path=folder_path,
+        repo_id=repo_id,
+        repo_type="model",
+    )
 
