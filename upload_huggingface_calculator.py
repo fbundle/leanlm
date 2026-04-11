802ffa6e48e43ba4a07e337275749b185166b052
khanh <khanh.nguyen.contact@gmail.com>
2026-04-12 02:10:31 +0800

    auto_commit

--- upload_huggingface_calculator.py
+++ upload_huggingface_calculator.py
@@ -1,7 +1,14 @@
 from huggingface_hub import login, upload_large_folder
+import datetime
+
 
 OUTPUT_DIR = "mnt/output/qwen3.5-4b-lora-calculator"
 REPO_ID = "khanh2023/qwen3.5-4b-lora-calculator"
+
+now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z")
+with open(f"{OUTPUT_DIR}/last_poll.txt", "w") as f:
+    f.write(now)
+
 login()
 upload_large_folder(
     folder_path=OUTPUT_DIR,
