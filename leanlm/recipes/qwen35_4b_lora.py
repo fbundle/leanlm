dfdbae4fd95661888c740ff60a2f44126fb61419
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-13 00:50:51 +0800

    auto_commit

--- leanlm/recipes/qwen35_4b_lora.py
+++ leanlm/recipes/qwen35_4b_lora.py
@@ -67,7 +67,7 @@ def main(main_mode: MainMode):
     eval_data = [generate_input(p, m) for _ in range(eval_size)]
 
     model_path = "Qwen/Qwen3.5-4B"
-    output_dir = f"mnt/output/qwen3.5-4b-length{max_completion_length}-lora-calculator"
+    output_dir = f"mnt/output/qwen3.5-4b-length{max_completion_length}-p{p}-lora-calculator"
     code_src_list = ["leanlm"]
     deepspeed = None # only for multi GPUs "conf/ds_zero2.json"
 
