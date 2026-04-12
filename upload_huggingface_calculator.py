5d813f9fdb0894504f0872cce5fad562288f2abf
Nguyen Ngoc Khanh <nguyenng@a2ap-login02.asp2p.nscc.sg>
2026-04-12 12:44:37 +0800

    auto_commit

--- upload_huggingface_calculator.py
+++ upload_huggingface_calculator.py
@@ -5,7 +5,7 @@ import datetime
 name = "qwen3.5-4b-length4096-lora-calculator"
 now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z")
 
-for name in ["qwen3.5-4b-length4096-lora-calculator", "qwen3.5-4b-length4096-calculator"]:
+for name in ["qwen3.5-4b-length4096-lora-calculator", "qwen3.5-4b-length4096-p0.3-calculator"]:
     folder_path = f"mnt/output/{name}"
     repo_id=f"khanh2023/{name}"
 
