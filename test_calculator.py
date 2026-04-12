c38db1cecfe50e37e99c4814b3e46bac7b2a7cb2
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-12 14:51:14 +0800

    auto_commit

--- test_calculator.py
+++ test_calculator.py
@@ -1,14 +1,25 @@
+import os
 from leanlm.llm_engine.api import ChatCompletionGenerateConfig
 from leanlm.llm_engine.engine import TransformerEngine
 from peft import PeftModel
 from transformers.trainer_utils import get_last_checkpoint
 
+def is_lora_checkpoint(path: str) -> bool:
+    return os.path.exists(os.path.join(path, "adapter_config.json"))
+
 def main():
 
-    # engine = TransformerEngine("mnt/output/qwen3.5-4b-length4096-calculator/checkpoint-1000")
-    engine = TransformerEngine("Qwen/Qwen3.5-4B")
-    engine.model = PeftModel.from_pretrained(engine.model, "mnt/output/qwen3.5-4b-length4096-lora-calculator/checkpoint-2400")
-    engine.model = engine.model.to("mps")
+    model_path = "Qwen/Qwen3.5-4B"
+    checkpoint_path = "mnt/output/qwen3.5-4b-length4096-calculator/checkpoint-1000"
+
+    if is_lora_checkpoint(checkpoint_path):
+        engine = TransformerEngine(model_path=model_path)
+        engine.model = PeftModel.from_pretrained(engine.model, checkpoint_path)  # type: ignore
+    else:
+        engine = TransformerEngine("mnt/output/qwen3.5-4b-length4096-calculator/checkpoint-1000")
+
+    engine.model = engine.model.to("mps") # type: ignore
+
 
     to_instruction = lambda input_text: "<|im_start|>user\n" + input_text + "<|im_end|>\n<|im_start|>assistant\n<think>\n"
     # to_instruction = lambda input_str: f"<｜begin▁of▁sentence｜><｜User｜>{input_str}<｜Assistant｜><think>\n"
