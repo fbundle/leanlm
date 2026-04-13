c482903d38b4d8239a2a90637be3f1f85c872e8b
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-13 17:51:21 +0800

    auto_commit

--- leanlm/recipes/qwen35_4b_lora_unsloth.py
+++ leanlm/recipes/qwen35_4b_lora_unsloth.py
@@ -26,7 +26,7 @@ def load_model_and_tokenizer(model_path: str, max_completion_length: int):
 
     model = FastLanguageModel.get_peft_model(
         model,
-        r=16,
+        r=8,
         target_modules=[
             "q_proj",
             "k_proj",
@@ -36,7 +36,7 @@ def load_model_and_tokenizer(model_path: str, max_completion_length: int):
             "up_proj",
             "down_proj",
         ],
-        lora_alpha=32,
+        lora_alpha=16,
         lora_dropout=0,  # Dropout = 0 is currently optimized
         bias="none",  # Bias = "none" is currently optimized
         use_gradient_checkpointing=True,
