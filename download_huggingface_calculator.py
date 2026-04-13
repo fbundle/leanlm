02b9d2494a01f6708723e0bd5085b5511875e490
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-13 09:57:43 +0800

    auto_commit

--- download_huggingface_calculator.py
+++ download_huggingface_calculator.py
@@ -32,9 +32,8 @@ def download_latest_checkpoint(local_dir: str, repo_id: str, path_in_repo: str =
 
 
 for name in [
-    "qwen3.5-4b-length4096-p0.3-phoenix-calculator",
-    "qwen3.5-4b-length4096-p0.3-calculator",
-    "qwen3.5-4b-length4096-p0.3-lora-calculator",
+    "qwen3.5-0.8b-length4096-p0.3-phoenix-calculator",
+    "qwen3.5-0.8b-length4096-p0.3-lora-calculator",
 ]:
     print("downloading", name)
     OUTPUT_DIR = f"mnt/output/{name}"
