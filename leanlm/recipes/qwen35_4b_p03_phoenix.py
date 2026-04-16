d3a20d25030432344500fc36fe12e569b47afdfa
Nguyen Ngoc Khanh <nguyenng@a2ap-login02.asp2p.nscc.sg>
2026-04-16 15:38:38 +0800

    auto_commit

--- leanlm/recipes/qwen35_4b_p03_phoenix.py
+++ leanlm/recipes/qwen35_4b_p03_phoenix.py
@@ -39,7 +39,7 @@ type MainMode = Literal["train", "prepare", "debug"]
 def main(main_mode: MainMode):
     # memory ~ batch_size x num_generations x max_completion_length^n
     batch_size = 1
-    num_generations = 8
+    num_generations = 4
     max_completion_length = 2048
 
     accumulation_steps = 32 // batch_size
