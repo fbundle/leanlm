1e977ae08c8a441b5614165787c643e705384d09
khanh <khanh.nguyen.contact@gmail.com>
2026-04-16 08:52:39 +0800

    auto_commit

--- upload_huggingface_calculator.py
+++ upload_huggingface_calculator.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 
 from huggingface_hub import login, upload_large_folder
 import datetime
@@ -9,7 +10,7 @@ user_id = "khanh2023"
 
 now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S%z")
 
-for name in os.listdir(output_dir):
+def upload(name: str):
     folder_path = f"{output_dir}/{name}"
     repo_id=f"{user_id}/{name}"
 
@@ -28,3 +29,13 @@ for name in os.listdir(output_dir):
     except FileNotFoundError as e:
         print(e)
 
+
+ts = []
+
+for name in os.listdir(output_dir):
+    t = Thread(target=upload, args=(name))
+    ts.append(t)
+    t.start()
+
+for t in ts:
+    t.join()
