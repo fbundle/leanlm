3e2a1245d03a3b17b8463314ba3dde85e9259a06
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-23 00:01:11 +0800

    auto_commit

--- leanlm/env_trainer/environment.py
+++ leanlm/env_trainer/environment.py
@@ -11,21 +11,6 @@ class Env(Protocol):
     def step(self, action: Action) -> Delta: ...
 
 # some examples below
-
-import re
-
-def get_last_integer(text):
-    """
-    Finds and returns the last sequence of digits in a string.
-    Returns None if no digits are present.
-    """
-    # Pattern: \d+ (digits) that are NOT followed by any other digits (?!.*\d)
-    pattern = r'(\d+)(?!.*\d)'
-    
-    match = re.search(pattern, text)
-    
-    return int(match.group(0)) if match else None
-
 class GuessEnv(Env):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -43,13 +28,23 @@ I will say if your guess is higher or lower than my number
 """
     
     def step(self, action: Action) -> Delta:
-        # use regex to get the last integer
-        guess = get_last_integer(action)
+        words = action.split()
+        if "GUESS" not in words:
+            self.alive = False
+            self.last_step_reward = 0
+            return f"can't find your guess"
         
+        guess_str = words[words.index("GUESS") + 1]
+
+        try:
+            guess = int(guess_str)
+        except ValueError:
+            guess = None
+
         if guess is None:
             self.alive = False
-            return f"can't find the number in your input"
-        
+            self.last_step_reward = 0
+            return f"can't find the number in your guess"
 
         f = lambda x: 1 / (1 + x) # map [0, inf) -> [1, 0)
         points = f(abs(self.target - guess))
@@ -62,7 +57,9 @@ I will say if your guess is higher or lower than my number
             self.alive = False
         
         # best_reward = maximum points over time
-        self.last_step_reward = max(0, points - self.best_reward)
-        self.best_reward += self.last_step_reward
-        
+        if points > self.best_reward:
+            self.last_step_reward = points - self.best_reward
+            self.best_reward = points
+        else:
+            self.last_step_reward = 0        
         return state_delta
