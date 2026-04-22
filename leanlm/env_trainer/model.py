60f79cee71010cdb985dde98fd340ecab84e609f
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-22 21:31:42 +0800

    auto_commit

--- leanlm/env_trainer/model.py
+++ leanlm/env_trainer/model.py
@@ -6,18 +6,16 @@ from jaxtyping import Int, Float
 import torch
 from torch import Tensor
 from torch.functional import F  # type: ignore
-from transformers import PreTrainedModel, PreTrainedTokenizerBase
-
-def collapse_eos_token(text: str, eos_token: str) -> str:
-    while text.endswith(eos_token + eos_token):
-        text = text[:-len(eos_token)]
-    return text
+from transformers import BatchEncoding, PreTrainedModel, PreTrainedTokenizerBase
 
 def collapse_eos_token_id(completion_ids: Int[Tensor, "n"], eos_token_id: int) ->  Int[Tensor, "n1"]:
-    while len(completion_ids) >= 2 and completion_ids[-1] == eos_token_id and completion_ids[-2] == eos_token_id:
-        completion_ids = completion_ids[:-1]
-    return completion_ids
-
+    indices: Int[Tensor, "k"] = (completion_ids == eos_token_id).nonzero(as_tuple=True)[0]
+    if len(indices) == 0: # no eos_token found
+        return completion_ids
+    # eos_token found
+    # [tok, tok, eos, eos, eos] -> [tok, tok, eos]
+    index: int = int(indices[0]) + 1
+    return completion_ids[:index]
 
 class Model:
     def __init__(
@@ -26,15 +24,16 @@ class Model:
         ):
         self.tokenizer = tokenizer
         self.model = model
+        self.eos_token_id = int(self.tokenizer.eos_token_id) # type: ignore
         self.generation_kwargs = {
             "max_new_tokens": 256,
-            "eos_token_id": [self.tokenizer.eos_token_id],
+            "eos_token_id": [self.eos_token_id],
         }
         if generation_kwargs is not None:
             self.generation_kwargs.update(generation_kwargs)
         
         self.tokenizer.padding_side = "left"
-        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.tokenizer.pad_token_id = self.eos_token_id
 
         # pop hard coded keys
         for key in ["input_ids", "attention_mask", "output_logits", "return_dict_in_generate"]:
@@ -42,13 +41,8 @@ class Model:
                 raise RuntimeError(f"generation_kwargs[{key}] must not be set")
         
     
-    def tokenizer_encode(self, input_text: str) -> Int[Tensor, "m"]:
-        i = self.tokenizer(
-            text=input_text,
-            return_tensors="pt",
-        )
-        input_ids = i.input_ids.sequeeze()
-        return input_ids
+    def tokenizer_encode(self, input_text: str) -> list[int]:
+        return self.tokenizer(input_text).input_ids
 
     def tokenizer_decode(self, completions_ids: Int[Tensor, "n"]) -> str:
         output_text = self.tokenizer.decode(completions_ids)
@@ -56,26 +50,34 @@ class Model:
         return output_text
 
 
-    def model_generate(
-        self, input_ids: Int[Tensor, "b m"], attention_mask: Int[Tensor, "b m"],
-    ) -> tuple[Int[Tensor, "b n"], Float[Tensor, "b n d"]]:
-        b1, m1 = input_ids.shape
-        o = self.model.generate(                                        # type: ignore
-            input_ids=input_ids.to(self.model.device),
-            attention_mask=attention_mask.to(self.model.device),
+    def model_generate(self, input_ids_list: list[list[int]]) -> list[tuple[list[int], Float[Tensor, "n d"]]]:
+        e: BatchEncoding = self.tokenizer.pad(
+            {"input_ids": input_ids_list},
+            padding=True,
+            return_tensors="pt",
+        ).to(self.model.device)
+
+        o = self.model.generate( # type: ignore
+            input_ids=e.input_ids,
+            attention_mask=e.attention_mask,
             output_logits=True,
             return_dict_in_generate=True,
             **self.generation_kwargs,
         )
-        logits: Float[Tensor, "b n d"] = torch.stack(o.logits, dim=1)
-        logprobs: Float[Tensor, "b n d"] = F.log_softmax(logits, dim=-1)
-        b2, n2, d2 = logits.shape
-        b3, mn3 = o.sequences.shape
-        assert b1 == b2 and b2 == b3 and m1 + n2 == mn3
+        logits_batch: Float[Tensor, "b n d"] = torch.stack(o.logits, dim=1)
+        logprobs_batch: Float[Tensor, "b n d"] = F.log_softmax(logits_batch, dim=-1)
+        b, n, d = logits_batch.shape
+        completions_ids_batch: Int[Tensor, "b n"] = o.sequences[:, -n:]
 
-        completions_ids: Int[Tensor, "b n"] = o.sequences[:, -n2:]
-        return completions_ids, logprobs
+        completions_ids_batch = completions_ids_batch.detach().cpu()
 
+        outputs: list[tuple[list[int], Float[Tensor, "n d"]]] = []
+        for i in range(b):
+            completions_ids: list[int] = collapse_eos_token_id(completions_ids_batch[i, :], self.eos_token_id).tolist()
+            logprobs = logprobs_batch[i, :len(completions_ids), :]
+            outputs.append((completions_ids, logprobs))
+        
+        return outputs
     
 if __name__ == "__main__":
     from transformers import AutoTokenizer, AutoModelForCausalLM
