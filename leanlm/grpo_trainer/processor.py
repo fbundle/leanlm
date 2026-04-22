5156b8ca52ab22a478b7a26fe07d3546fe1120a7
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-22 14:42:29 +0800

    auto_commit

--- leanlm/grpo_trainer/processor.py
+++ leanlm/grpo_trainer/processor.py
@@ -24,8 +24,6 @@ class Type1ProcessorConfig(BaseModel):
     begin_answer: str
     end_answer: str
 
-
-
 class Type1Processor(Processor):
     def __init__(self, config: Type1ProcessorConfig) -> None:
         super().__init__()
